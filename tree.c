0ff1d6e8d91d98a7553bb6ae3a16b20d45adc879
Matteo Mei <52063123+FattiMei@users.noreply.github.com>
2020-04-20 22:04:12 +0200

    Definito struttura albero

---
+++ tree.c
@@ -0,0 +1,29 @@
+#include <malloc.h>
+
+#include "token.h"
+#include "tree.h"
+
+struct Node *allocNode(struct Token token){
+	struct Node *new_node;
+
+	new_node = (struct Node *) malloc(sizeof(struct Node));
+	if(new_node != NULL){
+		new_node->token = token;
+		new_node->l_branch = NULL;
+		new_node->r_branch = NULL;
+	}
+
+	return new_node;
+}
+
+void printTree(struct Node *root){
+	if(root != NULL){
+		if(root->token.type == integer) printf("%d", root->token.value);
+		else if(root->token.type == comma){
+			printTree(root->l_branch);
+			putchar(',');
+			printTree(root->r_branch);
+		}
+	}
+	else printf("<!>");
+}
---
+++ tree.h
@@ -0,0 +1,16 @@
+#ifndef TREE_HEADER_INCLUDED
+#define TREE_HEADER_INCLUDED
+
+#include "token.h"
+
+struct Node{
+	struct Token token;
+	struct Node *l_branch,
+		    *r_branch;
+};
+
+struct Node *allocNode(struct Token token);
+
+void printTree(struct Node *root);
+
+#endif
