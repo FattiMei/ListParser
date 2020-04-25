bf13dad5e47eb600232ca218739952ad5e82cbab
Matteo Mei <teomei68@gmail.com>
2020-04-25 09:42:32 +0200

    Primitiva gestione degli errori

--- interface.c
+++ interface.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
 #include "interface.h"
+#include "list.h"
+#include "parser.h"
+#include "error.h"
+
+struct List *interface(char *input){
+	char *ptr = input;
+	struct List *head = NULL;
+
+	head = listParser(&ptr);
+
+	//soluzione temporanea, lancio un generico errore
+	ptr = firstNonSpace(ptr);
+
+	if(*ptr == '\0' && head != NULL) return head;
+	else{
+		printf("Sintax error\n");
+		return NULL;
+	}
+}
--- interface.h
+++ interface.h
@@ -1,6 +1,8 @@
 #ifndef INTERFACE_PROTOTYPES_INCLUDED
 #define INTERFACE_PROTOTYPES_INCLUDED
 
+#include "list.h"
 
+struct List *interface(char *input);
 
 #endif
